61263036abce30b0e05cfae2895075ba65c30082
vasistasarma <vasista@gmail.com>
2015-01-04 21:53:29 -0800

    Added List view for Customer Orders at waypoints
    In the Item List view, Added the Waypoints and Items to be delivered,
    Quantity and Price.
    The cosmetic issues needs to be fixed

--- FudfillRunner/src/com/fudfill/runner/slidingmenu/ItemsListFragment.java
+++ FudfillRunner/src/com/fudfill/runner/slidingmenu/ItemsListFragment.java
@@ -5,8 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import com.fudfill.runner.slidingmenu.model.CustomerOrderDetails;
+import com.fudfill.runner.slidingmenu.model.CustomerOrderListAdapter;
+import com.fudfill.runner.slidingmenu.model.CustomerWaypointDetails;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsListFragment extends Fragment {
+    private List<CustomerWaypointDetails> wayPoints;
 
 	public ItemsListFragment() {
 	}
@@ -17,7 +26,38 @@ public class ItemsListFragment extends Fragment {
 
 		View rootView = inflater.inflate(R.layout.fragment_items,
 				container, false);
-
+        initData();
+        ExpandableListView itemList = (ExpandableListView)rootView.findViewById(R.id.customerItemList);
+        itemList.setIndicatorBounds(5, 5);
+        CustomerOrderListAdapter custOrderAdapter= new CustomerOrderListAdapter(wayPoints, this.getActivity().getApplicationContext());
+        itemList.setIndicatorBounds(0,20);
+        itemList.setAdapter(custOrderAdapter);
 		return rootView;
 	}
+    //Adding random data for Testing
+    private void initData(){
+
+        wayPoints = new ArrayList<CustomerWaypointDetails>();
+        CustomerWaypointDetails wayPoint1 = createWaypoint("1", "Vasista", "300");
+        wayPoint1.setItemList(createItems("Item 1", 2, 100));
+
+        CustomerWaypointDetails wayPoint2 = createWaypoint("2", "Vivek", "1200");
+        wayPoint2.setItemList(createItems("Item 2", 4, 100));
+        wayPoints.add(wayPoint1);
+        wayPoints.add(wayPoint2);
+   }
+    private CustomerWaypointDetails createWaypoint(String order, String name, String price) {
+        return new CustomerWaypointDetails(order, name, price);
+    }
+    private List<CustomerOrderDetails> createItems(String itemName, int itemQty, int itemPrice) {
+        List<CustomerOrderDetails> result = new ArrayList<CustomerOrderDetails>();
+
+        for (int i=0; i < itemQty; i++) {
+            CustomerOrderDetails item = new CustomerOrderDetails(itemName, itemPrice, itemQty*itemPrice);
+            result.add(item);
+        }
+
+        return result;
+    }
+
 }
