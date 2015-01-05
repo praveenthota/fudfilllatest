9daa1030a362be24b9ab7e93ba332efe681b5d1a
praveenthota <praveen.thota@gmail.com>
2015-01-04 16:42:44 -0800

    Adding google play sdk

--- FudfillRunner/src/com/fudfill/runner/slidingmenu/MainActivity.java
+++ FudfillRunner/src/com/fudfill/runner/slidingmenu/MainActivity.java
@@ -1,8 +1,7 @@
 package com.fudfill.runner.slidingmenu;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.Configuration;
@@ -16,14 +15,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.fudfill.runner.slidingmenu.adapter.NavDrawerListAdapter;
 import com.fudfill.runner.slidingmenu.model.NavDrawerItem;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
 	private ActionBarDrawerToggle mDrawerToggle;
+
+    public GoogleMap gmap;
+    private static final int GPS_ERRORDIALOG_REQUEST=9001;
 
 	// nav drawer title
 	private CharSequence mDrawerTitle;
@@ -41,6 +49,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+        if(servicesOK())
+        {
+            Toast.makeText(this,"Ready to Map", Toast.LENGTH_SHORT).show();
+        }
 		setContentView(R.layout.activity_main);
 
 		mTitle = mDrawerTitle = getTitle();
@@ -130,6 +143,25 @@ public class MainActivity extends Activity {
 			displayView(position);
 		}
 	}
+
+    public boolean servicesOK()
+    {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(isAvailable == ConnectionResult.SUCCESS)
+        {
+            return true;
+        }
+        else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailable))
+        {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable,this,GPS_ERRORDIALOG_REQUEST);
+            dialog.show();
+        }
+        else
+        {
+            Toast.makeText(this,"Cannot Connect to Google Play Service", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
